66cd0c9285fe8dad963d58cc42ed285752db8759
sabri3487 <sabrina.viola3@gmail.com>
2016-07-16 16:34:10 -0300

    Commit inicial

---
+++ application/views/menu-inicial.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+<head>
+
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="author" content="SemiColonWeb" />
+
+	<!-- Stylesheets
+	============================================= -->
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="public/css/bootstrap.css" type="text/css" />
+	<link rel="stylesheet" href="public/css/style.css" type="text/css" />
+	<link rel="stylesheet" href="public/css/dark.css" type="text/css" />
+	<link rel="stylesheet" href="public/css/font-icons.css" type="text/css" />
+	<link rel="stylesheet" href="public/css/animate.css" type="text/css" />
+	<link rel="stylesheet" href="public/css/magnific-popup.css" type="text/css" />
+
+	<link rel="stylesheet" href="public/css/responsive.css" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+	<!-- Document Title
+	============================================= -->
+	<title>NetCop</title>
+
+</head>
+
+<body class="stretched">
+
+	<!-- Document Wrapper
+	============================================= -->
+	<div id="wrapper" class="clearfix">
+
+		<!-- Header
+		============================================= -->
+		<header id="header" class="full-header">
+
+			<div id="header-wrap">
+
+				<div class="container clearfix">
+
+					<div id="primary-menu-trigger"><i class="icon-reorder"></i></div>
+
+					<!-- Logo
+					============================================= -->
+					<div id="logo">
+						<a href="index.html" class="standard-logo" data-dark-logo="public/images/logo-dark.png"><img src="public/images/logo.png" alt="NetCop Logo"></a>
+						<a href="index.html" class="retina-logo" data-dark-logo="public/images/logo-dark@2x.png"><img src="public/images/logo@2x.png" alt="NetCop Logo"></a>
+					</div><!-- #logo end -->
+
+					<!-- Primary Navigation
+					============================================= -->
+					<nav id="primary-menu">
+
+						<ul>
+							<li class="current"><a href="index.html"><div>Inicio</div></a></li>
+							<li><a href="#"><div>Monitoreo</div></a>
+								<ul>
+									<li><a href="#"><div><i class="icon-stack"></i>En tiempo real</div></a>
+									<li><a href="widgets.html"><div><i class="icon-gift"></i>Histórico</div></a></li>
+									<li><a href="#"><div><i class="icon-umbrella"></i>Por período</div></a></li>
+								</ul>
+							</li>
+							<li><a href="shop.html"><div>Políticas</div></a>
+								<ul>
+									<li><a href="#"><div><i class="icon-stack"></i>Consulta de políticas</div></a>
+									<li><a href="widgets.html"><div><i class="icon-gift"></i>Nueva política</div></a></li>
+								</ul>
+							</li>
+							<li><a href="shop.html"><div>Clases de tráfico</div></a>
+								<ul>
+									<li><a href="#"><div><i class="icon-stack"></i>Consulta de clases de tráfico</div></a>
+									<li><a href="widgets.html"><div><i class="icon-gift"></i>Nueva clase de tráfico</div></a></li>
+								</ul>
+							</li>
+							<li><a href="shop.html"><div>Usuarios</div></a>
+								<ul>
+									<li><a href="#"><div><i class="icon-stack"></i>Consulta de usuarios</div></a>
+									<li><a href="widgets.html"><div><i class="icon-gift"></i>Nuevo usuario</div></a></li>
+								</ul>
+							</li>
+							<li><a href="shop.html"><div>Sistema</div></a>
+								<ul>
+									<li><a href="#"><div><i class="icon-stack"></i>En tiempo real</div></a>
+									<li><a href="widgets.html"><div><i class="icon-gift"></i>Histórico</div></a></li>
+									<li><a href="#"><div><i class="icon-umbrella"></i>Por período</div></a></li>
+								</ul>
+							</li>
+							<li><a href="shop.html"><div>Ayuda</div></a></li>
+						</ul>
+
+						<!-- Top Search
+						============================================= -->
+						<div id="top-search">
+							<a href="#" id="top-search-trigger"><i class="icon-search3"></i><i class="icon-line-cross"></i></a>
+							<form action="search.html" method="get">
+								<input type="text" name="q" class="form-control" value="" placeholder="Type &amp; Hit Enter..">
+							</form>
+						</div><!-- #top-search end -->
+
+					</nav><!-- #primary-menu end -->
+
+				</div>
+
+			</div>
+
+		</header><!-- #header end -->
+
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title">
+
+			<div class="container clearfix">
+				<h1>Optimizá tu Red</h1>
+			</div>
+
+		</section><!-- #page-title end -->
+
+		<!-- Content
+		============================================= -->
+		<section id="content">
+
+			<div class="content-wrap">
+
+				<div class="container clearfix">
+
+					<div class="col_full">
+						Texto de prueba
+					</div>
+				</div>
+			</div>
+
+		</section><!-- #content end -->
+
+		<!-- Footer
+		============================================= -->
+		<footer id="footer" class="dark">
+
+			<!-- Copyrights
+			============================================= -->
+			<div id="copyrights">
+
+				<div class="container clearfix">
+
+					<div class="col_half">
+						&copy; 2016 PowerSoft. Todos los Derechos Reservados.<br>
+					</div>
+				</div>
+
+			</div><!-- #copyrights end -->
+
+		</footer><!-- #footer end -->
+
+	</div><!-- #wrapper end -->
+
+	<!-- Go To Top
+	============================================= -->
+	<div id="gotoTop" class="icon-angle-up"></div>
+
+	<!-- External JavaScripts
+	============================================= -->
+	<script type="text/javascript" src="public/js/jquery.js"></script>
+	<script type="text/javascript" src="public/js/plugins.js"></script>
+
+	<!-- Footer Scripts
+	============================================= -->
+	<script type="text/javascript" src="public/js/functions.js"></script>
+
+
+</body>
+</html>
