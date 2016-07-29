a9f729957ce2943badfe91814bc9f2cec69935c9
julianpatricio <derosajulian@gmail.com>
2016-07-29 00:36:36 -0300

    arreglo estilos

--- application/views/usuario-nuevo.php
+++ application/views/usuario-nuevo.php
@@ -3,29 +3,26 @@
 
 <div> 
 	<div class="clear"></div>
-	<div class="col_full">
-		<div class="col_half">
-			<h3 class="marginUser">Nombre:</h3>
-			<input id="inpUser" class="sm-form-control" style="width:40%;">
+		<div class="col_one_third" style="margin-right:136px; margin-left:153px;">
+			<label>Nombre: </label>
+			<input id="inpUser" class="sm-form-control">
 		</div>
-		<div class="col_half col_last">
-			<h3 class="marginUser">Mail:</h3>
-			<input id="inpMail" class="sm-form-control" style="width:40%;">
+
+		<div class="col_one_third">
+			<label>Mail: </label>
+			<input id="inpMail" class="sm-form-control">
 		</div>
-	</div>
-	<div class="col_full">
-		<div class="col_half">
-			<h3 class="marginUser">Rol:</h3>
-			<select id="cboRol" class="select-1 form-control" style="width:40%; height:40px;">
+		<div class="col_one_third" style="margin-right:136px; margin-left:153px;">
+			<label>Rol:</label>
+			<select id="cboRol" class="select-1 form-control" style="height:40px;">
 				<option value="Administrador">ADMINISTRADOR</option>
 			    <option value="Monitor">MONITOR</option>			    
 			</select>
-		</div>
-		<div class="col_half col_last">
-			<h3 class="marginUser">Contraseña:</h3>
-			<input id="inpPass" class="sm-form-control" style="width:40%;">
-		</div>
-	</div>
+		</div>		
+		<div class="col_one_third">
+			<label>Contraseña:</label>
+			<input id="inpContraseña" class="sm-form-control" >
+		</div>	
 	<div class="col_full" style="text-align:center;">
 		<button class="button button-rounded">GUARDAR</button>
 		<button class="button button-rounded" style="background-color:#F44336;">CANCELAR</button>	
