b2316c255717fb222f84daa2840c3f1280975971
deperona <demianperona@gmail.com>
2016-10-10 21:06:46 -0300

    Update Ayuda.php

--- application/controllers/Ayuda.php
+++ application/controllers/Ayuda.php
@@ -1,12 +1,14 @@
 <?php
 error_reporting(E_ALL^E_NOTICE^E_WARNING);
 defined('BASEPATH') OR exit('No direct script access allowed');
+require_once("LoginRequired.php");
 
 class Ayuda extends CI_Controller {
 
 	public function __construct() {
 		parent::__construct();
         $this->load->helper('url');
+        $this->load->library('session');
 	}
 
     public function ayuda() {
