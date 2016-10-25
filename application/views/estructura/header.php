63d49f16105d78df703502d6cc10d119f2e9573a
sabri3487 <sabrina.viola3@gmail.com>
2016-10-24 22:20:52 -0300

    Se agregan fuentes para que no las tome de internet

--- application/views/estructura/header.php
+++ application/views/estructura/header.php
@@ -7,7 +7,7 @@
 
 	<!-- Stylesheets
 	============================================= -->
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="<?=base_url('public/css/fonts.css')?>" type="text/css"/>
 	<link rel="stylesheet" href="<?=base_url('public/css/bootstrap.css')?>" type="text/css" />
 	<link rel="stylesheet" href="<?=base_url('public/css/style.css')?>" type="text/css" />
 	<link rel="stylesheet" href="<?=base_url('public/css/dark.css')?>" type="text/css" />
